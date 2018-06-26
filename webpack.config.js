7a11f901d030d2db1784461b7632411c1b85c786
Maksim Kozhukh <mkozhukh@ya.ru>
2018-06-26 15:44:55 +0300

    [dev] jet 1.6, webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require("webpack");
 module.exports = function(env) {
 
 	var pack = require("./package.json");
-	var ExtractTextPlugin = require("extract-text-webpack-plugin");
+	var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 	var production = !!(env && env.production === "true");
 	var asmodule = !!(env && env.module === "true");
@@ -15,29 +15,33 @@ module.exports = function(env) {
 	};
 
 	var config = {
-		entry: "./sources/myapp.js",
+		mode: production ? "production" : "development",
+		entry: {
+			myapp: "./sources/myapp.js"
+		},
 		output: {
 			path: path.join(__dirname, "codebase"),
 			publicPath:"/codebase/",
-			filename: "myapp.js"
+			filename: "[name].js",
+			chunkFilename: "[name].bundle.js"
 		},
-		devtool: "inline-source-map",
 		module: {
 			rules: [
 				{
 					test: /\.js$/,
-					loader: "babel-loader?" + JSON.stringify(babelSettings)
+					use: "babel-loader?" + JSON.stringify(babelSettings)
 				},
 				{
 					test: /\.(svg|png|jpg|gif)$/,
-					loader: "url-loader?limit=25000"
+					use: "url-loader?limit=25000"
 				},
 				{
 					test: /\.(less|css)$/,
-					loader: ExtractTextPlugin.extract("css-loader!less-loader")
+					use: [ MiniCssExtractPlugin.loader, "css-loader", "less-loader" ]
 				}
 			]
 		},
+		stats:"minimal",
 		resolve: {
 			extensions: [".js"],
 			modules: ["./sources", "node_modules"],
@@ -47,22 +51,23 @@ module.exports = function(env) {
 			}
 		},
 		plugins: [
-			new ExtractTextPlugin("./myapp.css"),
+			new MiniCssExtractPlugin({
+				filename:"[name].css"
+			}),
 			new webpack.DefinePlugin({
 				VERSION: `"${pack.version}"`,
 				APPNAME: `"${pack.name}"`,
 				PRODUCTION : production,
 				BUILD_AS_MODULE : (asmodule || standalone)
 			})
-		]
+		],
+		devServer:{
+			stats:"errors-only"
+		}
 	};
 
-	if (production) {
-		config.plugins.push(
-			new  webpack.optimize.UglifyJsPlugin({
-				test: /\.js$/
-			})
-		);
+	if (!production){
+		config.devtool = "inline-source-map";
 	}
 
 	if (asmodule){
