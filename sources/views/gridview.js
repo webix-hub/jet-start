e7853905bb290aa6db4c41b21dc893023c05ca0d
AlenaMikalaeynaLisava <alenalisava87@gmail.com>
2021-04-21 18:57:35 +0300

    fix: change to localId

--- sources/views/gridview.js
+++ sources/views/gridview.js
@@ -1,44 +1,47 @@
-import {JetView} from "webix-jet";
+import { JetView } from "webix-jet";
 
 export default class GridView extends JetView {
-	constructor(app, name, data){
-		super(app, name); //call parent constructor
-		this._gridData = data; //define specific parameters
-	}
-	config(){
-		return {
-			rows:[
-				{view: "datatable",
-					editable: true,
-					autoConfig:true},
-				{ view:"toolbar", cols:[
-					{ view:"button", value:"Add new",
-						on:{ onItemClick:function(){
-							var table = this.getParentView().getParentView().queryView("datatable");
-							table.add({Name:"Alan"});
-						}
-						}
-					},
-					{ view:"button",  value:"Remove selected",
-						on:{ onItemClick:function(){
-							var table = this.getParentView().getParentView().queryView("datatable");
-							var sel = table.getSelectedId();
-							if(sel) table.remove(sel);
-						}
-						}},
-					{ view:"button", value:"Refresh",
-						on:{ onItemClick:function(){
-							var table = this.getParentView().getParentView().queryView("datatable");
-							table.refresh();
-						}
-						}
-					},
-				]}
-			]
-		};
-	}
-	init(view) {
-		view.queryView("datatable").parse(this._gridData);
-		view.queryView("datatable").getColumnConfig("Name").editor = "text";
-	}
+  constructor(app, name, data) {
+    super(app, name); //call parent constructor
+    this._gridData = data; //define specific parameters
+  }
+  config() {
+    return {
+      rows: [
+        {
+          view: "datatable",
+          editable: true,
+          localId: "mydatatable",
+          autoConfig: true,
+        },
+        {
+          view: "toolbar",
+          cols: [
+            {
+              view: "button",
+              localId: "addbutton",
+              value: "Add new",
+            },
+            {
+              view: "button",
+              localId: "removebutton",
+              value: "Remove selected",
+            },
+          ],
+        },
+      ],
+    };
+  }
+  init(view) {
+    let table = this.$$("mydatatable");
+    table.parse(this._gridData);
+    table.getColumnConfig("Name").editor = "text";
+    this.$$("addbutton").attachEvent("onItemClick", function () {
+      table.add({ Name: "Alan" });
+    });
+    this.$$("removebutton").attachEvent("onItemClick", function () {
+      var sel = table.getSelectedId();
+      if (sel) table.remove(sel);
+    });
+  }
 }
